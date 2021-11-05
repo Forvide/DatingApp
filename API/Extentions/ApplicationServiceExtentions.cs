3bc50b4b1a496936a8d5a4cb2434ce24a1376900
Murat <murat_a@mail.ru>
2021-11-05 13:53:13 +0600

    rerelease

--- API/Extentions/ApplicationServiceExtentions.cs
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -41,7 +41,7 @@ namespace API.Extentions
             else
             {
                 // Use connection string provided at runtime by Heroku.
-                var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                 // Parse connection URL to connection string for Npgsql
                 connUrl = connUrl.Replace("postgres://", string.Empty);
@@ -60,8 +60,8 @@ namespace API.Extentions
             // Whether the connection string came from the local development configuration file
             // or from the environment variable from Heroku, use it to set up your DbContext.
             options.UseNpgsql(connStr);
-                    });
-                    return services;
+            });
+            return services;
         }
     }
 }
